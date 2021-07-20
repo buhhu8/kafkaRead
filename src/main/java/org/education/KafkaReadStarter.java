745ea29dd460ca8537a435bc47739d7309fe6a14
Denis Dorofeev <buhhu8@yandex.ru>
2021-07-20 21:32:57 +0300

    Kafka consumer

---
+++ src/main/java/org/education/KafkaReadStarter.java
@@ -0,0 +1,23 @@
+package org.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaReadStarter {
+
+    @KafkaListener(topics="msg")
+    public void msgListener(String msg){
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KafkaReadStarter.class,args);
+
+    }
+
+}
---
+++ src/main/java/org/education/kafkaRead/Consumer.java
@@ -0,0 +1,15 @@
+//package org.education.kafkaRead;
+//
+//import org.springframework.kafka.annotation.EnableKafka;
+//import org.springframework.kafka.annotation.KafkaListener;
+//
+//
+//public class Consumer {
+//
+//
+//    @KafkaListener(topics = "msg")
+//    public void msgListener(String msg) {
+//        System.out.println(msg);
+//    }
+//
+//}
